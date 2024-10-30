bbdbe8ee80977cc2396cf2ef12211c801d7f0519
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-10-30 04:03:18 +0300

    added the footer section-website done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
       <ul className='app__navbar-links'>
         <li className='p__opensans'><a href="#home" >Home</a></li>
         <li className='p__opensans'><a href="#about">About</a></li>
-        <li className='p__opensans'><a href="#menu">Menu</a></li>
         <li className='p__opensans'><a href="#awards">Awards</a></li>
+        <li className='p__opensans'><a href="#gallery">Gallery</a></li>
         <li className='p__opensans'><a href="#contact">Contact Us</a></li>
       </ul>
 
